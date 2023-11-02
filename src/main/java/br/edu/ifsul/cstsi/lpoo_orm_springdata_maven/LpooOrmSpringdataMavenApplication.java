164debfe47b756bd579b0e255cae2aa6ff5eb739
Huandel Siqueira das Neves <122821151+huandelsiqueira@users.noreply.github.com>
2023-11-02 19:18:43 -0300

    Inicio do objetivo5 de lpoo com springdata

---
+++ src/main/java/br/edu/ifsul/cstsi/lpoo_orm_springdata_maven/LpooOrmSpringdataMavenApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.lpoo_orm_springdata_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpooOrmSpringdataMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LpooOrmSpringdataMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsul/cstsi/lpoo_orm_springdata_maven/LpooOrmSpringdataMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.lpoo_orm_springdata_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpooOrmSpringdataMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
